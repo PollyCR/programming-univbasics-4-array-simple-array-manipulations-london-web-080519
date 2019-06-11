069b49ed1ed56257cf80831a7c1b886cfec1855c
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-11 16:19:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,3 +17,15 @@ end
 def using_shift(array)
   return array.shift
 end 
+
+def shift_with_args(array)
+  array.shift(2)
+end 
+
+def using_concat(array1,array2)
+  array1.concat(array2)
+end 
+
+def using_iinsert(array,element)
+  array.insert[4](element)
+end 
