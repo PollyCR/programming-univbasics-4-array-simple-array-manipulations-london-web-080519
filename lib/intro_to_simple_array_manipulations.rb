f40b48a77a98bdef0eecf3e6eb94e158c89070ae
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-11 16:23:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,21 @@ end
 def using_insert(array,element)
   array.insert(4, element)
 end 
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten(array)
+  array.flatten!
+end 
+
+def using_delete(array,string)
+  array.delete(string)
+end 
+
+def using_delete(array,int)
+  array.delete[int]
+end 
+
+
